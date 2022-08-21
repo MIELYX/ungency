063999125111fefc3b56f981b92b03ee89ac2c5a
MIELYX <kacper_mielczarek@Kacpers-Air.lan>
2022-08-22 00:22:29 +0200

    the fotter was created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const hamburger = document.querySelector('.hamburger')
 const slideOutNav = document.querySelector('.nav__slide-out')
 const slideOutItems = document.querySelectorAll('.nav__slide-out .item')
+const currentYear = document.querySelector('.current-year')
+
+const handleCurrentYear = () => {
+	const year = (new Date).getFullYear()
+	currentYear.innerText = year;
+}
 
 const slideOut = () => {
 	slideOutNav.classList.toggle('slide-out')
 	document.body.classList.toggle('sticky-body')
 }
 
+handleCurrentYear()
 hamburger.addEventListener('click', slideOut)
 slideOutItems.forEach(item => item.addEventListener('click', slideOut))
