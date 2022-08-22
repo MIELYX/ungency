a511013d49f7fbef1dde8bd3fcf29cce718a12f6
MIELYX <kacper_mielczarek@Kacpers-Air.lan>
2022-08-22 13:53:23 +0200

    section works was created

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ const hamburger = document.querySelector('.hamburger')
 const slideOutNav = document.querySelector('.nav__slide-out')
 const slideOutItems = document.querySelectorAll('.nav__slide-out .item')
 const currentYear = document.querySelector('.current-year')
+const btnLeft = document.querySelector('.arrow-left')
+const btnRight = document.querySelector('.arrow-right')
+const itemList = document.querySelectorAll('.work-item')
 
 const handleCurrentYear = () => {
-	const year = (new Date).getFullYear()
-	currentYear.innerText = year;
+	const year = new Date().getFullYear()
+	currentYear.innerText = year
 }
 
 const slideOut = () => {
@@ -13,6 +16,28 @@ const slideOut = () => {
 	document.body.classList.toggle('sticky-body')
 }
 
+const slider = () => {
+	itemList.forEach(item => {
+		if (item.classList.contains('show')) {
+			item.classList.remove('show')
+		} else {
+			item.classList.add('show')
+		}
+	})
+}
+
+const swaper = () => {
+	itemList.forEach(item => {
+		if (item.classList.contains('swap')) {
+			item.classList.remove('swap')
+		} else {
+			item.classList.add('swap')
+		}
+	})
+}
 handleCurrentYear()
+btnLeft.addEventListener('click', slider)
+btnRight.addEventListener('click', slider)
 hamburger.addEventListener('click', slideOut)
 slideOutItems.forEach(item => item.addEventListener('click', slideOut))
+itemList.forEach(item => item.addEventListener('click', swaper))
